6f872b1fc3129f1797abe4f25ae5a8fe2f7007f0
OliverRoat <oliver.roat@gmail.com>
2024-10-21 15:05:58 +0200

    Fixed minor merge fuck up

--- src/pages/CarBrandPage.tsx
+++ src/pages/CarBrandPage.tsx
@@ -1,29 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import "../styles/CarBrandPage.css";
 import useBrands from "../hooks/useBrands";
-import logo from "../../public/Audi-logo.png"; // dette skal hentes fra databasen som en url
-
-// Disse skal være i en database og have links til dem fra backenden, når et brand hentes
-//const brands: { name: string; logo: string }[] = [
-//  { name: "BMW", logo: "/BMW-LOGO.png" },
-//  { name: "Mercedes", logo: "/Mercedes-logo.png" },
-//  { name: "Audi", logo: "/Audi-logo.png" },
-//  { name: "Skoda", logo: "/Skoda-logo.png" },
-//  { name: "Ford", logo: "/Ford-logo.png" },
-//];
 
 function CarBrandPage() {
   const { brands, error } = useBrands();
 
   const navigate = useNavigate();
 
-  // Handle errors
   if (error) {
     return <div>Error Message: {error}</div>;
-    return <div>Error Message: {error}</div>;
   }
-
-  console.log("Brands data:", brands); // Debugging output
 
   return (
     <div>
@@ -31,8 +17,6 @@ function CarBrandPage() {
       <div className="containerCarbrand">
         {brands.map((brand) => (
           <div
-            key={brand.name}
-            onClick={() => navigate(`/brands/${brand.name.toLowerCase()}`)}
             key={brand.id}
             onClick={() => navigate(`/brands/${brand.name.toLowerCase()}`)}
           >
@@ -40,8 +24,6 @@ function CarBrandPage() {
               className="logoCarbrand"
               src={brand.logo}
               alt={`${brand.name} logo`}
-              src={logo} // Use the same Audi logo for all
-              alt="Audi logo"
             />
             <p>{brand.name}</p>
           </div>
