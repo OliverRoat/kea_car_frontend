5ec24eed0dad34938e956641fd429703d410ce1e
OliverRoat <oliver.roat@gmail.com>
2024-11-06 09:38:00 +0100

    changes background color and fixed url in Color page

--- src/pages/CarColorPage.tsx
+++ src/pages/CarColorPage.tsx
@@ -1,20 +1,32 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import "../styles/CarColorPage.css";
 
+// Define the Color interface based on the structure you are using.
+interface Color {
+  id: string;
+  name: string;
+  price: number;
+  red_value: number;
+  green_value: number;
+  blue_value: number;
+}
+
 function CarColorPage() {
   const location = useLocation();
-  const { brand, modelId, modelName, colors } = location.state || {};
+  const { brand, modelName, colors } = location.state || {};
   const navigate = useNavigate();
 
   const handleColorSelect = (color: string) => {
-    navigate(`/brands/${brand}/models/${modelId}/colors/${color}/accessories`);
+    navigate(
+      `/brands/${brand}/models/${modelName}/colors/${color}/accessories`
+    );
   };
 
   return (
     <div>
       <h1>Select a Color for {modelName}</h1>
       <div className="color-grid">
-        {colors.map((color: any) => (
+        {colors.map((color: Color) => (
           <div
             key={color.id}
             className="color-item"
